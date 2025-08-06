f84ba938ead2dd23437554a14fa88f914bcf6e2b
Tiago Peres França <tiago.franca@zup.com.br>
2025-08-06 11:20:45 -0300

    extension finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,49 @@
-/**
- * Attention: this is just an example, we didn't use any library to control our form. Feel free to use
- * the library of your choice or no library at all.
- */
+import { Button, Column, FieldGroup, IconBox, IconButton, Input, Row, Text, Tooltip } from '@stack-spot/citric-react'
+import { cancel, getData, onChangeData, showToaster, StackspotExtension, submit } from '@stack-spot/portal-extension'
+import { useCallback, useEffect, useState } from 'react'
 
-import { StackspotExtension } from '@stack-spot/portal-extension'
-import { MyForm } from './MyForm'
+export const App = () => {
+  const [isLoading, setLoading] = useState(false)
+  const [pin, setPin] = useState('')
 
-export const App = () =>  <StackspotExtension><MyForm /></StackspotExtension>
+  useEffect(() => {
+    setPin(getData().pin)
+    onChangeData(data => setPin(data.pin))
+  }, [])
+
+  const generatePin = useCallback(async () => {
+    setLoading(true)
+    try {
+      const response = await fetch('https://lucas-moraes-stk.app.n8n.cloud/webhook/extension-widget-stk-demo', { method: 'post' })
+      if (!response.ok) throw new Error(`Error while generating pin: ${response.status}.\n${await response.text()}`)
+      const { pin } = await response.json()
+      if (!pin) throw new Error ('Unexpected response format from the backend')
+      setPin(pin)
+    } catch (error) {
+      console.error(error)
+      showToaster({ type: 'error', message: 'Unable to generate pin. Try again later.' })
+    } finally {
+      setLoading(false)
+    }
+  }, [])
+
+  return (
+    <StackspotExtension>
+      <Column gap="20px">
+        <Text appearance="h2">Gerador de Pin</Text>
+        <FieldGroup style={{ width: 'auto' }}>
+          <IconBox icon="Lock" />
+          <Input placeholder="Use o botão abaixo para gerar um pin" value={pin} disabled style={{ flex: 1 }} />
+          <Tooltip content="Copiar" position="left">
+            <IconButton icon="Copy" feedback="Copiado!" onClick={() => navigator.clipboard.writeText(pin)} />
+          </Tooltip>
+        </FieldGroup>
+        <Button onClick={generatePin} loading={isLoading}>Gerar Pin</Button>
+        <Row justifyContent="space-between">
+          <Button colorScheme="inverse" appearance="outlined" onClick={cancel}>Anterior</Button>
+          <Button onClick={() => submit({ pin })} disabled={!pin}>Próximo</Button>
+        </Row>
+      </Column>
+    </StackspotExtension>
+  )
+}
